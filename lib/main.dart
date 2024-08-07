7adc4ec652eb57eb41da42219a6e745ccff149e2
Jinwook Kim <redwoodnatlpark@gmail.com>
2024-08-07 17:03:18 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import "package:blog_web_app/screens/home_screen.dart";
 import "package:flutter/material.dart";
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: HomeScreen(),
     ),
   );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  WebViewController webViewController = WebViewController()
+    ..loadRequest(Uri.parse("https://blog.codefactory.ai"))
+    ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Home Screen"),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: const Text("Code Factory"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                webViewController
+                    .loadRequest(Uri.parse("https://blog.codefactory.ai"));
+              },
+              icon: const Icon(
+                Icons.home,
+              )),
+          IconButton(
+              onPressed: () {
+                webViewController.goBack();
+              },
+              icon: const Icon(Icons.chevron_left))
+        ],
+      ),
+      body: WebViewWidget(
+        controller: webViewController,
+      ),
     );
   }
 }
